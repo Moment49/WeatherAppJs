af30abea1ea0b4ddd3ab0283c8d04a8f3d5c3fd7
ibenacho Elvis <ibenachoelvis49@gmail.com>
2022-08-16 01:49:24 +0100

    updated

--- Introduction to JS/conditionals/script.js
+++ Introduction to JS/conditionals/script.js
@@ -30,29 +30,67 @@ const para = document.querySelector('h3');
 para.style.color = 'red' 
 para.style.textAlign = 'center'
 para.style.fontSize = '1.3rem'
+const sunny = document.getElementById("sunny");
+const rainy = document.getElementById("rainy");
+const snow = document.getElementById("snow");
 
 const changeWeather=()=>{
     let choice = select.value;
     let temperature = 46;
-    if(choice === 'sunny'){
-        if(temperature < 86){
-        para.innerHTML = `It's ${temperature} deg nice \n\
-    and sunny outside today. Wear shorts!\n\
-    Go to the beach, or the park and get an ice cream`;
-        }
-    }else if (choice === 'rainy'){
-        para.innerHTML = "Rain is falling outside;\n\
-        take a rain coat and an umbrella, and don't stay out for too long";  
-    }else if (choice === 'snowing'){
-        para.innerHTML = "The snow is coming down - it is freezing!;\n\
-        Best to stay in with a cup of hot chocolate, or go build a snowbuild"; 
-     
-    }else if (choice === 'overcast'){
-        para.innerHTML = "It isn't raining,; \n\
-        but the sky is grey and glommy; it could turn any minute,\n\
-        so take a rain coat just in case.";  
-    }else{
-        para.innerHTML = "Please select a weather type"
+
+    //Using switch statement to test the conditions
+    switch (choice || temperature < 86){
+        case 'sunny':
+            para.innerHTML = `It's ${temperature} deg nice \n\
+            and sunny outside today. Wear shorts!\n\
+            Go to the beach, or the park and get an ice cream`;
+            sunny.style.border = '1px solid white'
+            sunny.style.backgroundColor = '#121245'
+        
+            rainy.style.border = '1px solid white'
+            rainy.style.backgroundColor = ''
+        
+            snow.style.border = '1px solid white'
+            snow.style.backgroundColor = ''
+            break;
+        case 'rainy':
+            para.innerHTML = "Rain is falling outside;\n\
+            take a rain coat and an umbrella, and don't stay out for too long";
+            rainy.style.border = '1px solid white'
+            rainy.style.backgroundColor = '#121245'
+    
+            sunny.style.border = '1px solid white'
+            sunny.style.backgroundColor = ''
+    
+            snow.style.border = '1px solid white'
+            snow.style.backgroundColor = ''
+            break;
+        case 'snowing':
+            para.innerHTML = "The snow is coming down - it is freezing!;\n\
+            Best to stay in with a cup of hot chocolate, or go build a snowbuild"; 
+            snow.style.border = ''
+            snow.style.backgroundColor = '#121245'
+    
+            sunny.style.border = '1px solid white'
+            sunny.style.backgroundColor = '' 
+    
+            rainy.style.border = '1px solid white'
+            rainy.style.backgroundColor = ''   
+            break;
+        default:
+            para.innerHTML = "Please select a weather type"
+
+            snow.style.border = ''
+            snow.style.backgroundColor = ''
+    
+            sunny.style.border = '1px solid white'
+            sunny.style.backgroundColor = '' 
+    
+            rainy.style.border = '1px solid white'
+            rainy.style.backgroundColor = ''
+
+
     }
+ 
 }
 select.addEventListener('change', changeWeather);
